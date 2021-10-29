117fa312010644c4229d39db47d8a6546328da56
JoaoRosendo <joao.rafael_7@hotmail.com>
2021-10-29 12:26:39 +0100

    done?

--- Miniquest1.cpp
+++ Miniquest1.cpp
@@ -1,124 +1,180 @@
 #include <iostream>
 #include <string.h>
+#include <time.h>
 
 using namespace std;
 
-  char Map[10][11] = {
-    {'X','X','X','X','X','X','X','X','X','X','\0'},
-    {'X','H',' ',' ',' ',' ',' ',' ',' ','X','\0'},
-    {'X',' ','X','X',' ','X',' ','X',' ','X','\0'},
-    {'X','D','X','X',' ','X',' ','X',' ','X','\0'},
-    {'X',' ','X','X',' ','X',' ','X',' ','X','\0'},
-    {'X',' ',' ',' ',' ',' ',' ','X',' ','E','\0'},
-    {'X',' ','X','X',' ','X',' ','X',' ','X','\0'},
-    {'X',' ','X','X',' ','X',' ','X',' ','X','\0'},
-    {'X',' ','X','X',' ',' ',' ',' ',' ','X','\0'},
-    {'X','X','X','X','X','X','X','X','X','X','\0'}
-    };
+int keyInventory = 0;
 
 
-int moveHero(char KeyPress, char Map[10][11], int *PosX, int *PosY){
+int nearDragon(char Map[10][11], int PosX, int PosY)
+{
+    if      (Map[PosY+1][PosX] == 'D') return 1;
+    else if (Map[PosY-1][PosX] == 'D') return 1;
+    else if (Map[PosY][PosX+1] == 'D') return 1;
+    else if (Map[PosY][PosX-1] == 'D') return 1;
+    else return 0;
+}
 
-    switch(KeyPress) {
+void keyGenerator(char Map[10][11])
+{
+    int PosX, PosY;
 
-        case 'w' :
-        case 'W' :
-        {   
-            if(Map[*PosY-1][*PosX] != 'X')
-            {
-                Map[*PosY][*PosX]   = ' ';
-                Map[*PosY-1][*PosX] = 'H';
-                *PosY = *PosY-1;
-                
-            }
+    do
+    {
+        PosX = rand() % 10;
+        PosY = rand() % 10;
 
-            break;
-        }
+    }
 
-        case 'a' :
-        case 'A' :
+    while ((Map[PosY][PosX] != ' ') || (nearDragon(Map, PosX, PosY)));
+
+    Map[PosY][PosX] = 'K';
+}
+
+int nextMove(char Map[10][11], int *currentX, int *currentY, int nextX, int nextY)
+
+{
+
+    if (Map[nextY][nextX] == 'X')
+    {
+    }
+    else if (Map[nextY][nextX] == 'K')
+    {
+        Map[*currentY][*currentX] = ' ';
+        Map[nextY][nextX] = 'H';
+        *currentX = nextX;
+        *currentY = nextY;
+        keyInventory = 1;
+    }
+    else if (Map[nextY][nextX] == ' ')
+    {
+        Map[*currentY][*currentX] = ' ';
+        Map[nextY][nextX] = 'H';
+        *currentX = nextX;
+        *currentY = nextY;
+    }
+    else if (Map[nextY][nextX] == 'E')
+    {
+        if (keyInventory == 0)
         {
-            if(Map[*PosY][*PosX-1] != 'X')
-            {
-                Map[*PosY][*PosX]   = ' ';
-                Map[*PosY][*PosX-1] = 'H';
-                *PosX = *PosX-1;
-            }
-
-            break;
         }
-
-        case 's' :
-        case 'S' :
+        else if (keyInventory == 1)
         {
-             if(Map[*PosY+1][*PosX] != 'X')
-            {
-                Map[*PosY][*PosX]   = ' ';
-                Map[*PosY+1][*PosX] = 'H';
-                *PosY = *PosY+1;
-            }
-            
-            break;
+            Map[nextY][nextX] = ' ';
         }
-        
-        case 'd' :
-        case 'D' :
-        {   
-             if(Map[*PosY][*PosX+1] != 'X')
-            {
-                Map[*PosY][*PosX]   = ' ';
-                Map[*PosY][*PosX+1] = 'H';
-                *PosX = *PosX+1;
-            }
-            break;
-        }
+    }
+    return 0;
 
+}
+
+int moveHero(char KeyPress, char Map[10][11], int *PosX, int *PosY)
+{
+
+    int nextX, nextY;
+
+    switch (KeyPress)
+    {
+
+    case 'w':
+    case 'W':
+    {
+        nextMove(Map, PosX, PosY, *PosX, *PosY - 1);
+        break;
+    }
+
+    case 'a':
+    case 'A':
+    {
+        nextMove(Map, PosX, PosY, *PosX - 1, *PosY);
+        break;
+    }
+
+    case 's':
+    case 'S':
+    {
+        nextMove(Map, PosX, PosY, *PosX, *PosY + 1);
+        break;
+    }
+
+    case 'd':
+    case 'D':
+    {
+        nextMove(Map, PosX, PosY, *PosX + 1, *PosY);
+        break;
+    }
     }
     return 0;
 }
 
+int printMap(char Map[10][11])
+{
 
-
-
-int printMap(char Map[10][11]){
-
-    for(int i = 0; i<10; i++)
+    for (int i = 0; i < 10; i++)
     {
-        for(int x = 0; x<10; x++){
+        for (int x = 0; x < 10; x++)
+        {
 
             std::cout << Map[i][x];
             std::cout << " ";
         }
-    
+
         std::cout << "\n";
     }
 
     return 0;
 }
 
+int gameOver(char Map[10][11], int PosX, int PosY)
+{
+    if (nearDragon(Map, PosX, PosY))
+    {
+        cout << "\n\n\n\n\n\nMorreste\n\n\n\n\n\n\n";
+        return 1;
+    }
 
+    else if (Map[5][9] == 'H')
+    {
+        cout << "\n\n\n\n\n\nVitória\n\n\n\n\n\n\n";
+        return 1;
+    }
 
+    else return 0;
 
-int main() {
+}
+int main()
+{
+    srand(time(NULL));
 
     char KeyPress;
 
     int PosX = 1;
     int PosY = 1;
 
-   
+    char Map[10][11] = {
+        {'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', '\0'},
+        {'X', 'H', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', '\0'},
+        {'X', ' ', 'X', 'X', ' ', 'X', ' ', 'X', ' ', 'X', '\0'},
+        {'X', 'D', 'X', 'X', ' ', 'X', ' ', 'X', ' ', 'X', '\0'},
+        {'X', ' ', 'X', 'X', ' ', 'X', ' ', 'X', ' ', 'X', '\0'},
+        {'X', ' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', 'E', '\0'},
+        {'X', ' ', 'X', 'X', ' ', 'X', ' ', 'X', ' ', 'X', '\0'},
+        {'X', ' ', 'X', 'X', ' ', 'X', ' ', 'X', ' ', 'X', '\0'},
+        {'X', ' ', 'X', 'X', ' ', ' ', ' ', ' ', ' ', 'X', '\0'},
+        {'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', '\0'}};
 
-    
+    keyGenerator(Map);
+    printMap(Map);
 
-   while(1){
-        //cout << "Press a key";
+    while (1)
+    {   
+        
+        if(gameOver(Map, PosX, PosY)) break;
         cin >> KeyPress;
-        //cout << KeyPress;
         cout << "\n";
         moveHero(KeyPress, Map, &PosX, &PosY);
         printMap(Map);
-        printf("%d %d",PosX,PosY);
-        
-   }
+        printf("%d %d", PosX, PosY);
+    }
     return 0;
 }
